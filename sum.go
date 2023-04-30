c5634b4b0baa4ce1574321f3febe06a9bf79b766
THIAGO PEREIRA DOS SANTOS <thiago.pereira@tatic.com.net>
2023-04-29 21:17:21 -0400

    add

--- sum.go
+++ sum.go
@@ -3,3 +3,15 @@ package main
 func Sum(a int, b int) int {
 	return a + b
 }
+func sub(a int, b int) int {
+	return a - b
+}
+func mult(a int, b int) int {
+	return a * b
+}
+func div(a int, b int) int {
+	return a / b
+}
+func igualdade(a int, b int) int {
+	return a + b
+}
